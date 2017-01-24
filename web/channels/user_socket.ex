44cc64cd4fd42e5f706dafc5f73d83cc910d129b
Logan Moore <logan.moore@trineo.co.nz>
2017-01-24 16:20:45 +1300

    Support Heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule FitbitLeaderboard.UserSocket do
   # channel "room:*", FitbitLeaderboard.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
