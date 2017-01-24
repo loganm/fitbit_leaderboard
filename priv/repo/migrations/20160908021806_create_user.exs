6c5db6b5595e273546fa315ea1e9361f81428365
Logan Moore <logan.moore@trineo.co.nz>
2017-01-25 11:11:25 +1300

    Make user_id unique

--- priv/repo/migrations/20160908021806_create_user.exs
+++ priv/repo/migrations/20160908021806_create_user.exs
@@ -6,12 +6,13 @@ defmodule FitbitLeaderboard.Repo.Migrations.CreateUser do
       add :name, :string
       add :email, :string
       add :user_id, :string
-      add :access_token, :binary
       add :refresh_token, :binary
+      add :access_token, :binary
       add :expires_at, :datetime
 
       timestamps()
     end
 
+    create unique_index(:users, [:user_id])
   end
 end
